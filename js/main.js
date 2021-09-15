bdcb1ead8f6f5c918b59d4caa7d911980520fec7
danyamamin99 <50888464+danyamamin99@users.noreply.github.com>
2021-09-15 15:59:06 +0300

    add files star_wars

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    navClass: ["nav-prev", "nav-next"],
+    responsive:{
+        0:{
+            items:1
+        },
+        700:{
+            items:2
+        },
+        900:{
+            items:3
+        },
+        1200: {
+            items:4
+        }
+    }
+})
